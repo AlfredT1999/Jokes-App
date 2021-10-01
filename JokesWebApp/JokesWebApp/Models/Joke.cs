bf6b63fea19d0415393a0c44e5dab01f55a4467f
alfred <alfredtapia1999@outlook.com>
2021-10-01 15:06:41 -0500

    This commit corresponds to implements the Search Jokes Feature, by doing this first I develop the add items to Nav bar. This is the 1 part.

--- JokesWebApp/JokesWebApp/Models/Joke.cs
+++ JokesWebApp/JokesWebApp/Models/Joke.cs
@@ -1,12 +1,16 @@
-﻿namespace JokesWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JokesWebApp.Models
 {
     public class Joke
     {
         // These section corresponds to the properties of the current class:
         public int Id { get; set; }
 
+        [Display(Name = "The Joke")]
         public string JokeQuestion { get; set; }
 
+        [Display(Name = "The Answer To The Joke")]
         public string JokeAnswer { get; set; }
 
         // Constructor:
